b9caff188412e85d2744602b1b7ce9d40aed52e5
MIRJAKARIYA <mirjakariya239@gmail.com>
2022-05-03 03:44:54 +0600

    banner done

--- src/components/Banner/BannerComponent/BannerComponent.js
+++ src/components/Banner/BannerComponent/BannerComponent.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './BannerComponent.css';
 const BannerComponent = ({image,Des}) => {
     return (
-        <div className='banner-component-style p-8 flex flex-col md:flex-row items-center'>
-            <div className='w-1/2 h-full'>
+        <div className='banner-component-style p-2 md:p-8 flex flex-col md:flex-row items-center'>
+            <div className='md:w-1/2 h-full'>
                 <img src={image} alt="" className='w-full h-full object-cover rounded-lg banner-img-style' />
             </div>
-            <div className='w-1/2 ml-10'>
-                <h1 className='text-left text-5xl font-bold'>{Des?.title}</h1>
-                <p className='text-left text-lg mt-5'>{Des?.des}</p>
+            <div className='md:w-1/2 md:ml-10'>
+                <h1 className='text-left text-2xl mt-3 md:mt-0 md:text-5xl font-bold'>{Des?.title}</h1>
+                <p className='text-left text-sm md:text-lg md:mt-5'>{Des?.des}</p>
             </div>
         </div>
     );
